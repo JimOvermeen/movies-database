e01c4c977c6179eef8db859a25fc35860ce34771
Jim Overmeen <67960046+JimOvermeen@users.noreply.github.com>
2024-11-28 15:48:55 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,1 @@
-export const environment = {
-    production: false,
-    randomMoviesApiUrl: 'https://moviesdatabase.p.rapidapi.com/titles/random?list=most_pop_movies',
-    XRapidAPIHostLabel: 'X-RapidAPI-Host',
-    XRapidAPIHostValue: 'moviesdatabase.p.rapidapi.com',
-    XRapidAPIKeyLabel: 'X-RapidAPI-Key',
-    XRapidAPIKeyValue: '5ff069bd1fmshcf29bca29f2dce6p1b9930jsn53908529ca1c',
-    searchMoviesApiUrl: 'https://moviesdatabase.p.rapidapi.com/titles/search/keyword/',
-}
+
